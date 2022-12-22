68e1d5da0eab054b9412cc3a818d1ae49cbd7af2
D1saster20 <yuriizhukevych@gmail.com>
2022-12-22 13:25:34 +0200

    1

---
+++ main.js
@@ -0,0 +1,115 @@
+// function aandb (a, b){
+//    let result = a * b
+//     console.log(result)
+//    return result
+// }
+// aandb(40,30)
+
+// function circle (p, r){
+//     let result = p * r*r
+//     console.log(result)
+//     return result
+// }
+// circle(3.14, 10 )
+
+// function cylinder (h,r){
+//     let p = 3.14
+//     let result = p * r*r*h
+//     console.log(result)
+//     return result
+// }
+// cylinder(10,10)
+
+// let users = [
+//     {name: 'vasya', age: 31, status: false},
+//     {name: 'petya', age: 30, status: true},
+//     {name: 'kolya', age: 29, status: true},
+//     {name: 'olya', age: 28, status: false},
+//     {name: 'max', age: 30, status: true},
+//     {name: 'anya', age: 31, status: false},
+//     {name: 'oleg', age: 28, status: false},
+//     {name: 'andrey', age: 29, status: true},
+//     {name: 'masha', age: 30, status: true},
+//     {name: 'olya', age: 31, status: false},
+//     {name: 'max', age: 31, status: true}
+// ];
+// function printarray (array){
+//     for (const item of array) {
+//         console.log(item)
+//     }
+// }
+// printarray(users)
+
+// function text (word){
+//    document.write(`<p>${word}</p>`)
+// }
+// text('fnejfnewn')
+
+// function ul(lishku){
+//     document.write(
+//     `
+//         <ul>
+//             <li>${lishku}</li>
+//             <li>${lishku}</li>
+//             <li>${lishku}</li>
+//         </ul>
+//     `)
+// }
+// ul('fnenfjenj')
+
+// function asd (text, number){
+//     for (let i = 0; i < number; i++) {
+//         document.write(
+//             `<ul>
+//             <li>${text}</li>
+//             </ul>
+//             `
+//         )
+//     }
+// }
+// asd('ffbebhb',4)
+
+// let x = [334,566,false,'fhkefkehkh'];
+// function array (x){
+//     document.write(`<ul>`)
+//     for (const item of x) {
+//       document.write(`<li>${item}</li>`)
+//     }
+//     document.write(`</ul>`)
+// }
+// array(x);
+
+// let user = {name:'alex',age:23,id:0}
+//
+// function poe (user){
+//     document.write(
+//         `<div>${user.id}</div>
+//         <div>${user.name}</div>
+//         <div>${user.age}</div>
+//         `
+//     )
+// }
+// poe(user)
+
+// let number = [12,434,443,212,6]
+// function minimum (number){
+//     let min = 6;
+//     for (const item of number) {
+//         if (item<min){
+//             min = item
+//         }
+//     }
+//     return min
+// }
+// console.log(minimum(number))
+
+// let number = [3,4,5]
+// function sum (number){
+//     let summa = 0
+//     for (const item of number) {
+//         summa += item
+//
+//     }return summa
+// }
+//
+// console.log(sum(number))
